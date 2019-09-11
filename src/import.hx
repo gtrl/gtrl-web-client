e4cfc3c7224be8bd2cd3c94d6db6baba31fa09b9
tong <tong@disktree.net>
2019-09-12 01:05:34 +0200

    Cleanup

--- src/import.hx
+++ src/import.hx
@@ -1,18 +1,17 @@
 
 import haxe.Timer;
-import Std.int;
 
 import js.Browser.console;
 import js.Browser.document;
 import js.Browser.navigator;
 import js.Browser.window;
-import js.Error;
-import js.Promise;
 import js.html.ButtonElement;
 import js.html.CanvasElement;
 import js.html.DivElement;
 import js.html.Element;
 import js.html.InputElement;
+import js.lib.Error;
+import js.lib.Promise;
 
 import om.ArrayTools;
 import om.Json;
